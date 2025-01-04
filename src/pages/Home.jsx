b7bd5426e56be2723e5b12886efe1c2a6ea0b0f4
Andre Cadete <andrecadete70@gmail.com>
2025-01-04 19:51:27 +0100

    setting up the routes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (  
         <>
-            <h1>Home</h1>
+            <h1 className="text-3xl font-bold underline">Home</h1>
+
+            <div>
+                <Link to={'/workouts'}>
+                    <button>Workouts</button>
+                </Link >
+            </div>
+            <div>
+                <Link to={'/profile'}>
+                    <button>Profile</button>
+                </Link>
+            </div>
         </>
     );
 }
